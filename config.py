cc7235854a08a669ddd52daf747fb515f5812002
Colin Daniels <colindaniels@Colins-Air.nandor.net>
2021-01-03 19:45:45 -0600

    Better bs4 usage. Cleaned up. Added comments. Added ability for other devices to be searched

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
-models = ['se', '6', '6-plus', '6s', '6s-plus', '7', '7-plus', '8', '8-plus', 'x', 'xr', 'xs', 'xs-max', 'se-2nd-gen', '11', '11-pro', '11-pro-max']
-
+iPhones = {'web-prefix': 'mobile',
+           'devices': ['iphone-se', 'iphone-6', 'iphone-6-plus', 'iphone-6s', 'iphone-6s-plus', 'iphone-7', 'iphone-7-plus', 'iphone-8', 'iphone-8-plus', 'iphone-x', 'iphone-xr', 'iphone-xs', 'iphone-xs-max', 'iphone-se-2nd-gen', 'iphone-11', 'iphone-11-pro', 'iphone-11-pro-max'],
+           'type': '<td>'}
+apple_watches = {'web-prefix': 'wearables',
+                 'devices': ['watch-1st-gen-38mm', 'watch-1st-gen-42mm', 'watch-se-40mm', 'watch-se-44mm', 'watch-series-5-40mm'],
+                 'type': '<h2>'}
 
 company_percentage = 0.2
 #buying fees
@@ -9,7 +13,7 @@ buying_shipping = 8.61 #flat
 ebay_fee = 0.076
 sales_tax = 0.0625
 
-carrier_depreciation = {'Unlocked': 1, 'Verizon': 1, 'AT&T': 0.962, 'T-Mobile': 0.93, 'Sprint': 0.893, 'Other': 0.5}
+carrier_depreciation = {'Unlocked': 1, 'Verizon': 1, 'AT&T': 0.962, 'T-Mobile': 0.93, 'Sprint': 0.893, 'Other': 0.65}
 condition_depreciation = {'New': 1.25, 'Like_new': 1.06, 'Good': 1, 'Fair': 0.85, 'Broken': 0.5}
 
 
--- main.py
+++ main.py
@@ -4,43 +4,67 @@ import csv
 import math
 import config
 
-# exponential function that accounts for a price I will be garunteed to sell at and profit margins when buy prices are low or high
+# Declaring lists from config.py
+iPhones = config.iPhones
+apple_watches = config.apple_watches
+
+# All prices before variations
+phone_prices = []
+
+# Buy prices using the calculate_buy function
+buy_prices = []
+
+# Sell Prices using the calc_sell_price function
+sell_prices = []
+
+# Exponential function that accounts for a price that will be guaranteed to sell at and profit margins when buy prices are low or high
 def reliable_price(price):
     return 1-(1/(math.sqrt(price)+1)*3)
 
+# Changes price values to be put in buy_prices.csv
 def calculate_buy(data):
     data.append((model, storage, carrier, condition, (round(reliable_price(float(price)) *
                                                                          float(price)*config.carrier_depreciation[carrier] *
                                                                          config.condition_depreciation[condition], 2))))
 
+# Calculates new prices from buy_prices to sell_prices
 def calc_sell_price(row):
     model, storage, carrier, condition, price = row
-    price = float(price)           # str -> float
+    price = float(price)
     price = price-((price * config.ebay_fee) + (price * config.sales_tax) + (price * config.company_percentage)) - config.buying_shipping          # calculation
-    price = str(round(price,2) )   # float -> str
+    price = str(round(price,2) )
     return [model, storage, carrier, condition, price]
 
+#scrapes models and prices from swappa.com
+def scrape(device):
+    for progress, variation in enumerate(device['devices']):
+        url = f"https://swappa.com/sell/{device['web-prefix']}/apple-{variation}?carrier=unlocked"
+        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+        page = requests.get(url, headers=headers)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        # If the prices are found in a <td> tag
+        if device['type'] == '<td>':
+            model = soup.find('td').text.strip()
+            price = soup.find_all('td')[1].text.strip()
+            phone_prices.append([model, price])
+        # If the prices are found in a <h2> tag
+        elif device['type'] == '<h2>':
+            # Finds title of product and removes the "Get more green." string after name.
+            model = soup.find('h1').text.strip()[:-16]
+            # Finds price of product at the 3rd index of <h2>
+            price = soup.find_all('h2')[3].text.strip()
+            phone_prices.append([model, price])
+            print(model, price)
+        print(f"{round(progress/len(iPhones['devices'])*100, 1)}% Complete")
+    print('100% Complete')
 
-iPhones = config.models
-phone_prices = [] #all prices before variations
-buy_prices = [] #all price variations
-sell_prices = []
+scrape(iPhones)
 
-for progress, phone in enumerate(iPhones):
-    url = f"https://swappa.com/sell/mobile/apple-iphone-{phone}?carrier=unlocked"
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-    page = requests.get(url, headers=headers)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    tds = [td.text.replace("\t","").replace("\n","") for td in soup.find_all('td')]     # clean out all the junk
-    pps = [[f"{phone}", price] for phone, price in zip(tds[::2], tds[1::2])] # zip two alternating tds together: [phone, price]
-    phone_prices = phone_prices + pps
-    print(f'{round(progress/len(iPhones)*100, 1)}% Complete')
-print('100% Complete')
 with open("buy_prices.csv", 'w') as file:
     file.write('model,storage,price\n')
-    for n, (phone, price) in enumerate(phone_prices, start=1):
+    for n, (device, price) in enumerate(phone_prices, start=1):
         price = int(price[1:])
-        file.write(phone.replace(" ","") + ',' + str(price) + '\n')
+        file.write(device.replace(" ","") + ',' + str(price) + '\n')
     file.close()
 
 # Get starting prices
@@ -69,11 +93,3 @@ with open('sell_prices.csv', 'w') as sell_file:
             sell_file.write(','.join(row)+'\n')
         buy_file.close()
     sell_file.close()
-
-# original colde
-# with open('sell_prices.csv', 'w') as file:
-#     file.write('model,storage,carrier,condition,price\n')
-#     writer = csv.writer(file, delimiter=',')
-#     for row in sell_prices:
-#         writer.writerow(row)
-#     file.close()
